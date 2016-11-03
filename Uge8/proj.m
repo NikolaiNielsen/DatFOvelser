548824a968e2e3e62fe21ec1a2fa463b0789a617
nikolainielsen <NikolaiNielsen@outlook.dk>
2016-11-03 16:21:45 +0100

    now with english comments!

--- Uge8/proj.m
+++ Uge8/proj.m
@@ -3,31 +3,31 @@ close all
 clc
 
 % Variable!
-dt = 1;				% Tidsskridtet
-vis = 90;			% Synsfelt i grader.
-stor = [100 100]; 	% Spillebrættets størrelse
-ptime = 0.1;		% Pausetid
-tend = 100;
-dot_size = 15;
+dt = 1;				% time step
+vis = 90;			% fov in degrees
+stor = [100 100]; 	% size of the board
+ptime = 0.1;		% pausetime
+tend = 100;			% number of iterations
+dot_size = 15;		% Size of the dots for the scatter plot
 
 % For race 1:
-n1 = 25;			% Antal individer af race 1
-v_f1 = 0.5;			% Normal hastighed for race 1
-t_f1 = 10;			% Antal tidsskridt, som race 1 bevæger sig i en given retning
-r_panik = 10;		% Panikafstand for race 1. Skal være mindre end r_f2
-t_panik = 15;		% Paniktid for race 1
-t_pp = 10;			% Sekundær panik (panik fra at se et andet individ i panik)
-r_die1 = 5;			% Afstand mellem r1 og r2, hvor race 1 dør
-r.col(1) = 'g';		% Farven af individer
+n1 = 25;			% number of individuals of race 1
+v_f1 = 0.5;			% Normal speed for race 1
+t_f1 = 10;			% num steps a race 1 cell moves in a given direction
+r_panik = 10;		% panic distance for race 1, must be smaller than r_f2
+t_panik = 15;		% panic time for race 1
+t_pp = 10;			% secondary panic time
+r_die1 = 5;			% distance between race 1 and 2, where race 1 dies
+r.col(1) = 'g';		% color of healthy race 1 cells
 
 % For race 2:
-n2 = 50;			% Antal individer af race 2
-v_f2 = 0.2;			% Normal hastighed for race 2
-t_f2 = 3;			% ligeledes for race 2.
-r_f2 = 15;			% Spotradius for race 2.
-t_fol = 10;			% Tidsskridt race 2 forfølger i.
-r_die2 = 5;			% Afstand hvor r2 dør.
-r.col(2) = 'r';		% Farven af individer
+n2 = 50;			% num cells race 2
+v_f2 = 0.2;			% Normal speed for race 2
+t_f2 = 3;			% time for race 2
+r_f2 = 15;			% vision radius for race 2.
+t_fol = 10;			% steps an r2 cell follows an r1 cell
+r_die2 = 5;			% Distance at which r2 dies, if backstabbed
+r.col(2) = 'r';		% color of r2 cells
 
 r.resetTime = [t_f1; t_f2];
 r.Hast = [v_f1; v_f2];
@@ -46,32 +46,39 @@ for i = 2:tend
 	rv = rvec(r.pos(:,:,i-1));
 
 
-	% Vi tæller ned i antallet af trin til retningsskift
+	% counting down the time variable towards 0 (where a change in
+	% direction happens)
 	r.t = r.t-1;
 
-
-	% Tjekker hvilke der er 0
+	% Logic array of which cells need updated velocities
 	r.tally(1,:) = r.t == 0;
 
-	% Finder ny retning for disse, og opdater hastigheden
+	% update the direction and velocity
 	r.vel(:,:) = velupdate(r);
 
-	% Genstarter tiden for disse
+	% restart time counter
 	r.t = tid(r);
 
-	% Ny position. Simpel Eulerintegration
+	% new position for individuals. Simple Euler integration
 	r.pos(1,r.r1,i) = r.pos(1,r.r1,i-1) + r.vel(1,r.r1)*dt;
 	r.pos(2,r.r1,i) = r.pos(2,r.r1,i-1) + r.vel(2,r.r1)*dt;
 	r.pos(1,r.r2,i) = r.pos(1,r.r2,i-1) + r.vel(1,r.r2)*dt;
 	r.pos(2,r.r2,i) = r.pos(2,r.r2,i-1) + r.vel(2,r.r2)*dt;
 
-	% Positionen tjekkes, for om randen nås:
+	% Edgecases are checked. If they meet the edge, they're reflected
 	r.oob = r.pos(:,:,i) <= 0 | r.pos(:,:,i) >= stor(1);
 	if ~isempty(r.oob)
+		% A matrix of 1's and -1's to indicate which velocity components
+		% needs to be flipped is used to update the velocity.
+		% r.oob is a logical array, and r.oob*(-2)+1 has -1 for flipped
+		% components, and 1 for non-flipped components
 		r.vel = r.vel.*(r.oob*(-2)+1);
+		% update the position with new velocity vectors.
 		r.pos(:,:,i) = r.pos(:,:,i-1) + r.vel*dt;
 	end
 
+	% Delete the last iterations scatter plots, plot the new ones and pause
+	% the simulation.
 	delete(p1);
 	delete(p2);
 	p1 = scatter(r.pos(1,r.r1,i),r.pos(2,r.r1,i),dot_size,r.col(1),'filled');
